38e86ddd64d5e169e0985bea27b7deb1820f1aa9
sayyidnaqvi <naqvi.sdsu@gmail.com>
2025-11-06 10:44:38 -0600

    Material3 added

--- composeApp/src/commonMain/kotlin/learn/plcoding/mychirp/App.kt
+++ composeApp/src/commonMain/kotlin/learn/plcoding/mychirp/App.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import learn.plcoding.core.designsystem.theme.ChirpTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,7 +23,8 @@ import mychirp.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+//    MaterialTheme {
+    ChirpTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
